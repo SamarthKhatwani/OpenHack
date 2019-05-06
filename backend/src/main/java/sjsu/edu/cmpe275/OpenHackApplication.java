3307f64a0e993e75eae76740556d96cc7cfe8654
Samarth Khatwani <SamarthKhatwani47@gmail.com>
2019-05-06 04:00:38 -0700

    changes to authentication aspect

--- backend/src/main/java/sjsu/edu/cmpe275/OpenHackApplication.java
+++ backend/src/main/java/sjsu/edu/cmpe275/OpenHackApplication.java
@@ -1,7 +1,6 @@
 package sjsu.edu.cmpe275;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
@@ -11,9 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.UserRecord;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "sjsu.edu.cmpe275" })
@@ -30,16 +26,16 @@ public class OpenHackApplication {
 					  .setDatabaseUrl("https://cmpe-256-open-hack.firebaseio.com")
 					  .build();
 
-					FirebaseApp.initializeApp(options);
-					UserRecord user=null;
-					try {
-						user = FirebaseAuth.getInstance().getUser("buk5J9nvmmMbMy3BOUJlR9V0ySo1");
-					} catch (FirebaseAuthException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					String name=user.getEmail();
-					System.out.println(name);
+			FirebaseApp.initializeApp(options);
+//					UserRecord user=null;
+//					try {
+//						user = FirebaseAuth.getInstance().getUser("buk5J9nvmmMbMy3BOUJlR9V0ySo1");
+//					} catch (FirebaseAuthException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//					String name=user.getEmail();
+//					System.out.println(name);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ backend/src/main/java/sjsu/edu/cmpe275/api/controller/aspects/NormalAuthAuthenticationAspect.java
@@ -0,0 +1,50 @@
+package sjsu.edu.cmpe275.api.controller.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+
+import sjsu.edu.cmpe275.api.resources.ResponseMessage;
+
+
+@Aspect
+@Order(0)
+@Configuration
+public class NormalAuthAuthenticationAspect {
+
+	/**
+	 * validates the user token received from client
+	 * @param joinPoint
+	 * @return
+	 */
+	@Around("execution(* sjsu.edu.cmpe275.api.controller.interfaces.INormalAuthAPI.*(..))")
+	public ResponseEntity<Object> checkAuthenticationAspect(ProceedingJoinPoint joinPoint) throws Throwable{
+		try {	
+			Object[] args=joinPoint.getArgs();
+			String headerToken =(String)args[0];
+			FirebaseToken verifiedToken=FirebaseAuth.getInstance().verifyIdToken(headerToken);
+			if(verifiedToken==null) {
+				return new ResponseEntity<Object>(new ResponseMessage(false, "User is not authorized" ), HttpStatus.UNAUTHORIZED);
+			}
+			return (ResponseEntity<Object>) joinPoint.proceed();
+			}
+			catch(IllegalArgumentException e) {
+				e.printStackTrace();
+				return new ResponseEntity<Object>(new ResponseMessage(false, "User is not authorized"), HttpStatus.UNAUTHORIZED);
+			}
+			catch (FirebaseAuthException e) {
+				e.printStackTrace();
+				return new ResponseEntity<Object>(new ResponseMessage(false, "User is not authorized"), HttpStatus.UNAUTHORIZED);
+			}
+			
+	}
+
+}

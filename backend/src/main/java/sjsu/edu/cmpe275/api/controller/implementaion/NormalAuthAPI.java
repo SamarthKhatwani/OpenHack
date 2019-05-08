32b87a11397154b4582a62df6e14a66eb0d8714d
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2019-05-08 01:30:56 -0700

    list hackathon changes

--- backend/src/main/java/sjsu/edu/cmpe275/api/controller/implementaion/NormalAuthAPI.java
+++ backend/src/main/java/sjsu/edu/cmpe275/api/controller/implementaion/NormalAuthAPI.java
@@ -1,5 +1,6 @@
 package sjsu.edu.cmpe275.api.controller.implementaion;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,11 +10,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import sjsu.edu.cmpe275.api.controller.interfaces.INormalAuthAPI;
+import sjsu.edu.cmpe275.api.persistence.model.Hackathon;
 import sjsu.edu.cmpe275.api.persistence.model.Organization;
 import sjsu.edu.cmpe275.api.persistence.model.Profile;
+import sjsu.edu.cmpe275.api.persistence.model.mapper.HackathonToHackathonReponseMapper;
 import sjsu.edu.cmpe275.api.persistence.model.mapper.OrganizationToOrganizationByNameResponseMapper;
 import sjsu.edu.cmpe275.api.persistence.model.mapper.OrganizationToOrganizationResponseMapper;
 import sjsu.edu.cmpe275.api.persistence.model.mapper.ProfileToProfileResponseMapper;
+import sjsu.edu.cmpe275.api.resources.AllHackathonResponse;
+import sjsu.edu.cmpe275.api.resources.HackathonResponse;
 import sjsu.edu.cmpe275.api.resources.OrganizationByNameResponse;
 import sjsu.edu.cmpe275.api.resources.OrganizationMembershipRequest;
 import sjsu.edu.cmpe275.api.resources.OrganizationMembershipResponse;
@@ -23,6 +28,7 @@ import sjsu.edu.cmpe275.api.resources.OrganizationResponse;
 import sjsu.edu.cmpe275.api.resources.ProfileRequest;
 import sjsu.edu.cmpe275.api.resources.ProfileResponse;
 import sjsu.edu.cmpe275.api.resources.ResponseMessage;
+import sjsu.edu.cmpe275.api.service.intefaces.IHackathonManagementService;
 import sjsu.edu.cmpe275.api.service.intefaces.IOrganizationManagementService;
 import sjsu.edu.cmpe275.api.service.intefaces.IProfileManagementService;
 
@@ -31,24 +37,31 @@ public class NormalAuthAPI implements INormalAuthAPI {
 
 	@Autowired
 	private IProfileManagementService profileManagementService;
-	
+
 	@Autowired
 	private IOrganizationManagementService organizationManagementService;
-	
+
 	@Autowired
 	private ProfileToProfileResponseMapper profileToProfileResponseMapper;
-	
+
+	@Autowired
+	private HackathonToHackathonReponseMapper hackathonReponseMapper;
+
+	@Autowired
+	private IHackathonManagementService hackathonManagementService;
+
 	@Autowired
 	private OrganizationToOrganizationByNameResponseMapper organizationToOrganizationByNameResponseMapper;
-	
+
 	@Autowired
 	private OrganizationToOrganizationResponseMapper organizationToOrganizationResponseMapper;
-	
+
 	@Override
 	public ResponseEntity<Object> getProfile(String token, String email) {
 		Profile profile = profileManagementService.getProfile(email);
-		if(profile == null) {
-			return new ResponseEntity<Object>(new ResponseMessage(false,"Profile with given email doesn't exist" ), HttpStatus.NOT_FOUND);
+		if (profile == null) {
+			return new ResponseEntity<Object>(new ResponseMessage(false, "Profile with given email doesn't exist"),
+					HttpStatus.NOT_FOUND);
 		}
 		ProfileResponse response = profileToProfileResponseMapper.map(profile);
 		return new ResponseEntity<Object>(response, HttpStatus.OK);
@@ -57,15 +70,16 @@ public class NormalAuthAPI implements INormalAuthAPI {
 	@Override
 	public ResponseEntity<Object> updateProfile(String token, ProfileRequest profileRequest) {
 		Profile profile = profileManagementService.updateProfile(profileRequest);
-		if(profile != null ) {
+		if (profile != null) {
 			ProfileResponse response = profileToProfileResponseMapper.map(profile);
 			return new ResponseEntity<Object>(response, HttpStatus.OK);
-			
-		}else {
-			return new ResponseEntity<Object>(new ResponseMessage(false,"Profile with given email doesn't exist" ), HttpStatus.NOT_FOUND);
+
+		} else {
+			return new ResponseEntity<Object>(new ResponseMessage(false, "Profile with given email doesn't exist"),
+					HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
 	@Override
 	public ResponseEntity<Object> searchOrganization(String token, String name) {
 		List<Organization> organizations = organizationManagementService.getOrganizationByName(name);
@@ -79,7 +93,7 @@ public class NormalAuthAPI implements INormalAuthAPI {
 		OrganizationResponse response = organizationToOrganizationResponseMapper.map(organization);
 		return new ResponseEntity<Object>(response, HttpStatus.OK);
 	}
-	
+
 	@Override
 	public ResponseEntity<Object> listRequestOrganization(String token, String email) {
 		List<Organization> myOrganizations = organizationManagementService.getOwnedOrganizations(email);
@@ -101,13 +115,27 @@ public class NormalAuthAPI implements INormalAuthAPI {
 		}
 		OrganizationMembershipResponse membershipResponse = new OrganizationMembershipResponse();
 		membershipResponse.setSuccess(true);
-		if(response.isEmpty()) {
+		if (response.isEmpty()) {
 			membershipResponse.setMessage("No entries found");
-		}else {
+		} else {
 			membershipResponse.setMessage("Successful");
 		}
 		membershipResponse.setOrganization(response);
 		return new ResponseEntity<>(membershipResponse, HttpStatus.OK);
+	}
+
+	@Override
+	public ResponseEntity<Object> listHackathon(String token, String email, String role) throws ParseException {
+		List<HackathonResponse> hackathonResponse = new ArrayList<>();
+		List<Hackathon> hackathons = hackathonManagementService.retrieveHackathon(email, role);
+		for(Hackathon hackathon: hackathons) {
+			hackathonResponse.add(hackathonReponseMapper.map(hackathon, false, false, false));
+		}
+		AllHackathonResponse response = new AllHackathonResponse();
+		response.setSuccess(true);
+		response.setMessage("Successful");
+		response.setResults(hackathonResponse);
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
 	}
 
 }
--- backend/src/main/java/sjsu/edu/cmpe275/api/persistence/repository/HackathonRepository.java
+++ backend/src/main/java/sjsu/edu/cmpe275/api/persistence/repository/HackathonRepository.java
@@ -1,11 +1,17 @@
 package sjsu.edu.cmpe275.api.persistence.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import sjsu.edu.cmpe275.api.persistence.model.Hackathon;
 
 public interface HackathonRepository extends CrudRepository<Hackathon, Long> {
+
 	public Optional<Hackathon> findByEventName(String eventName);
+
+	@Query(value = "SELECT * FROM Hackathon h WHERE h.start_date = ?1 or h.event_name in ?2", nativeQuery=true)
+	public List<Hackathon> findHackathonBeforeStartAndNameIn(String date, List<String> names);
 }

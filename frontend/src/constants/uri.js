b3e492d71c9e226ad0cda251fc6e9e369fa98a57
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2019-05-21 14:10:05 -0700

    adding new aws instances info

--- frontend/src/constants/uri.js
+++ frontend/src/constants/uri.js
@@ -3,7 +3,7 @@ const URI = {
       //BASE_URL : "http://localhost:8080",
     // BASE_URL : "http://10.0.0.196:8080",
  //   BASE_URL: "http://10.0.0.87:8080",
-    BASE_URL : "http://ec2-18-191-93-151.us-east-2.compute.amazonaws.com:8080",
+    BASE_URL : "http://ec2-18-222-232-226.us-east-2.compute.amazonaws.com:8080",
     LOOKUP_SCREEN_NAME: "/lookupScreenName",
     SIGNUP: "/signup",
     GET_PROFILE: "/getProfile",
